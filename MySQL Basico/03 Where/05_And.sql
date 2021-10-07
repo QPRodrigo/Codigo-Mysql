c3fd892b70989f5c308ba15f2de4dad2d9886286
Rodrigo QP <rodrigoqp0722@gmail.com>
2021-10-07 16:35:30 -0500

    05 AND

---
+++ MySQL Basico/03 Where/05_And.sql
@@ -0,0 +1,38 @@
+/* AND */
+
+/*
+SELECT DISTINCT
+    select_list
+FROM
+    table_name
+WHERE 
+    search_condition
+AND
+    search_condition;
+*/
+
+
+-- Para encontrar clientes que se ubican en California (CA), EE. UU:
+SELECT 
+    customername, 
+    country, 
+    state
+FROM
+    customers
+WHERE
+    country = 'USA' AND 
+    state = 'CA';
+
+-- Devuelve los clientes que se encuentran en California, EE. UU., Y tienen un límite de crédito superior a 100K.
+SELECT 
+    customername, 
+    country, 
+    state, 
+    creditlimit
+FROM
+    customers
+WHERE
+    country = 'USA' AND 
+    state = 'CA' AND 
+    creditlimit > 100000;
+    
