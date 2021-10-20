aa4c40d1f367c1aa52d969652d117f2357593548
Rodrigo QP <rodrigoqp0722@gmail.com>
2021-10-20 00:38:26 -0500

    24 Derived Tables

---
+++ MySQL Basico/06 Subqueries/24_Derived Tables.sql
@@ -0,0 +1,90 @@
+/* Derived Tables */
+
+--  Para simplificar consultas complejas.
+
+/*
+
+SELECT 
+    select_list
+FROM
+    (SELECT 
+        select_list
+    FROM
+        table_1) derived_table_name
+WHERE 
+    derived_table_name.c1 > 0;
+
+*/
+
+### Un ejemplo simple de tabla derivada de MySQL
+-- La siguiente consulta obtiene los cinco productos principales por ingresos por ventas en 2003 de las tablas ordersy orderdetailsen la base de datos de muestra
+
+SELECT 
+    productCode, 
+    ROUND(SUM(quantityOrdered * priceEach)) sales
+FROM
+    orderdetails
+        INNER JOIN
+    orders USING (orderNumber)
+WHERE
+    YEAR(shippedDate) = 2003
+GROUP BY productCode
+ORDER BY sales DESC
+LIMIT 5;
+
+-- Puede usar el resultado de esta consulta como una tabla derivada y unirlo con la productstabla de la siguiente manera
+SELECT 
+    productName, sales
+FROM
+    (SELECT 
+        productCode, 
+        ROUND(SUM(quantityOrdered * priceEach)) sales
+    FROM
+        orderdetails
+    INNER JOIN orders USING (orderNumber)
+    WHERE
+        YEAR(shippedDate) = 2003
+    GROUP BY productCode
+    ORDER BY sales DESC
+    LIMIT 5) top5products2003
+INNER JOIN
+    products USING (productCode);
+
+### Un ejemplo de tabla derivada de MySQL más complejo
+-- Para formar esta consulta, primero debe colocar a cada cliente en el grupo respectivo usando la CASEexpresión y la GROUP BYcláusula de la siguiente manera
+SELECT 
+    customerNumber,
+    ROUND(SUM(quantityOrdered * priceEach)) sales,
+    (CASE
+        WHEN SUM(quantityOrdered * priceEach) < 10000 THEN 'Silver'
+        WHEN SUM(quantityOrdered * priceEach) BETWEEN 10000 AND 100000 THEN 'Gold'
+        WHEN SUM(quantityOrdered * priceEach) > 100000 THEN 'Platinum'
+    END) customerGroup
+FROM
+    orderdetails
+        INNER JOIN
+    orders USING (orderNumber)
+WHERE
+    YEAR(shippedDate) = 2003
+GROUP BY customerNumber;
+
+-- Luego, puede usar esta consulta como tabla derivada y realizar la agrupación de la siguiente manera:
+SELECT 
+    customerGroup, 
+    COUNT(cg.customerGroup) AS groupCount
+FROM
+    (SELECT 
+        customerNumber,
+            ROUND(SUM(quantityOrdered * priceEach)) sales,
+            (CASE
+                WHEN SUM(quantityOrdered * priceEach) < 10000 THEN 'Silver'
+                WHEN SUM(quantityOrdered * priceEach) BETWEEN 10000 AND 100000 THEN 'Gold'
+                WHEN SUM(quantityOrdered * priceEach) > 100000 THEN 'Platinum'
+            END) customerGroup
+    FROM
+        orderdetails
+    INNER JOIN orders USING (orderNumber)
+    WHERE
+        YEAR(shippedDate) = 2003
+    GROUP BY customerNumber) cg
+GROUP BY cg.customerGroup;    
